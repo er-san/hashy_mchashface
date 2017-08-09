0619b68b0ce5ee695f91e0791125bb40eee1e461
eUstundag <smokecode@gmail.com>
2017-08-08 21:37:11 -0400

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+    get '/', to: redirect('/search')
+    post '/search' => 'hashed_passwords#search'
+    get '/search' => 'hashed_passwords#search'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
