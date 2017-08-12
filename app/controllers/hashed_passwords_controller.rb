41f5ebc9f238408dc34216b0dcfdfc34edfc0692
er-san <smokecode@gmail.com>
2017-08-11 22:17:37 -0400

    update controller to digest sha1 for string

--- app/controllers/hashed_passwords_controller.rb
+++ app/controllers/hashed_passwords_controller.rb
@@ -4,15 +4,16 @@ class HashedPasswordsController < ApplicationController
   end
 
   def search
-  end
-
-  def show
-    hashed_string = Digest::SHA1.hexdigest(params[:password])
-    hashy_mchash = HashedPassword.where(hash_value: hashed_string)
-    if hashy_mchash.empty?
-      flash[:notice] = "Not found"
-    else
-      flash[:notice] = "Found"
+    flash[:found] = ''
+    if params[:password] && params[:password][:value]
+      hashed_string = Digest::SHA1.new.update(params[:password][:value]).to_s.upcase
+      hashy_mchash = HashedPassword.where(hash_value: hashed_string)
+      if hashy_mchash.empty?
+        flash[:found] = "Pool on the roof must have a leak. (Password not found)"
+      else
+        flash[:found] = "Mess with the best. Die like the rest. (Password found)"
+      end
     end
+    render 'search'
   end
 end
